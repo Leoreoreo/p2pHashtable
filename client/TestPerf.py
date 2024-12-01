7758a164a89d12a0520d1a74361e118ed03b2649
Leyang Li <lli27@student10.cse.nd.edu>
2024-11-30 21:35:28 -0500

    fix routing issue

---
+++ client/TestPerf.py
@@ -0,0 +1,20 @@
+# TestPerf
+
+import sys
+from SpreadSheetClient import SpreadSheetClient
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python3 TestPerf.py <project_name>")
+        sys.exit(1)
+    project_name = sys.argv[1]
+    client = SpreadSheetClient(project_name)
+
+    print(client.insert(114, {"weight": 100, "animal": "zebra"}))
+    print(client.lookup(114))
+    print(client.lookup(115))
+
+    print(client.remove(114))
+    print(client.lookup(114))
+
+    # print(client.invalid_req("afgrewa"))
